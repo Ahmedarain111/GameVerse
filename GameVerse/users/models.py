079d9e29555caa2fd7887a4b3c86866d44290c58
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-11-27 17:56:42 +0500

    Bug fixes

--- GameVerse/users/models.py
+++ GameVerse/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.Charfield(max_length=150, unique=True)
+    username = models.CharField(max_length=150, unique=True)
     
     profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
@@ -13,7 +13,7 @@ class User(AbstractUser):
     played_games = models.ManyToManyField('games.Game', related_name='played_by', blank=True)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username"]
     
     def __str__(self):
         return self.email
