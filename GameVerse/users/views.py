59d515afccdc678e31b384f689f58a0b9591548e
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-11-27 18:05:27 +0500

    Add basic views

--- GameVerse/users/views.py
+++ GameVerse/users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, authenticate, login, logout
 
-# Create your views here.
+
+User = get_user_model()
+
+
+def signup_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("pasword")
+        user = User.objects.create_user(email=email, password=password)
+
+        login(request, user)
+        return redirect("dashboard")
+
+    return render(request, "users/signup.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(request, email=email, password=password)
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
