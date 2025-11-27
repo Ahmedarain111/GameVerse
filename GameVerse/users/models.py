6d8edfca87549fd64330d8cfbd903eec317a4ccc
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-11-27 17:39:35 +0500

    Create user model

--- GameVerse/users/models.py
+++ GameVerse/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.Charfield(max_length=150, unique=True)
+    
+    profile_pic = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    
+    liked_games = models.ManyToManyField('games.Game', related_name='liked_by', blank=True)
+    wishlist = models.ManyToManyField('games.Game',     related_name='wishlisted_by', blank=True)
+    played_games = models.ManyToManyField('games.Game', related_name='played_by', blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+    
