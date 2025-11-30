33b6a340ea394d2d3fb6acd894f94038948f8fb5
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-11-30 20:23:52 +0500

    Add games model

--- GameVerse/games/models.py
+++ GameVerse/games/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.URLField(max_length=500)
+    release_year = models.IntegerField(blank=True, null=True)
+    platforms = models.TextField(blank=True, null=True)
+    steam_link = models.URLField(blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
+    rating_max = models.FloatField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
